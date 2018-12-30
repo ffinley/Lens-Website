f168aee663e6aecd5d0272a089d44374dd07d8f6
Jason <40499386+ffinley@users.noreply.github.com>
2018-12-30 21:13:29 +0800

    Add files via upload

---
+++ lens_earth/artworks.js
@@ -0,0 +1,204 @@
+ var artwoeks = [
+	{
+		"src":"static/img/01/1.jpg",
+		"longitude": -50.000,
+		"latitude": 75.000,
+		"title": "冰川",
+		"equipment":'Canon 1D MARK II + EF 200-400mm IS USM EXTENDER',
+		"origin": "格陵兰岛",
+		"author": "Jan Erik Waider",
+		"description": ""
+
+
+	},
+	{
+		"src":"static/img/02/1.jpg",
+		"longitude": 90.000,
+		"latitude": 35.000,
+		"title": "高原",
+		"equipment":'Nikon D610 + TAMRON SP 15-300mm Model A012',
+		"origin": "青藏高原",
+		"author": "张磊",
+		"description": "雄踞'世界屋脊'的青海省是个神秘而诱人的地方，她仿佛是一块未经雕琢的玉石，粗拙中透出珠光宝气，平静中显出神奇风采。距今六，七千年前，古代先民们就繁衍生息在这片土地上。斗转星移，沧桑变换。以古墓群，古寺庙，古岩画，古城堡为特征的名胜古迹众多。汉，藏，回，蒙古，土，哈萨克，撒拉等民族都有着悠久的历史和优秀的文化传统，保持着独特的，丰富多彩的民族风情和习俗。让我们一起跟随摄影师的脚步，去到这个质朴的广袤高原，洗涤一下自己的心灵。    （作者：张磊2013-07-18）"
+	},
+	{
+		"src":"static/img/03/1.jpg",
+		"longitude": 25.000,
+		"latitude": 71.000,
+		"title": "极光",
+		"equipment":'Pentax K1 + HD DFA 70~200mm F2.8',
+		"origin": "挪威",
+		"author": "Steven Henriksen",
+		"description": "Steven Henriksen是一位在摄影上自学成才的摄影师，1977出生在丹麦，后来定居在挪威诺德兰的Kabelvåg。Steven Henriksen主要关注的是风光摄影，并且对于风光摄影的追求可以用不辞辛劳来形容，他所拍摄的照片在社交网站上已经有超过两万名粉丝。下面这组照片是Steven Henriksen在挪威的罗佛敦群岛所拍摄的奇特的极光现象，让我们跟随Steven Henriksen的镜头一起去感受一下自然的奇特。"
+	},
+	{
+		"src":"static/img/04/1.jpg",
+		"longitude": -74.000,
+		"latitude": 40.000,
+		"title": "雨中纽约",
+		"equipment":'Canon 80D + EF 16-35mm f/2.8L III USM',
+		"origin": "纽约",
+		"author": "Dave Krugman",
+		"description": "摄影师Dave Krugman带来的这组照片清新无比，记录了纽约这个本来就缤纷多彩的城市在大雨中的样子。雨落在城市的马路上好像一层细纱，匆忙的片刻混淆着繁忙的都市街头让每一瞬间都变得清晰无比。纽约的季节与上海十分相似，目前也进入春雨季的上海淅淅沥沥，从作品的氛围中或许也能感受到一丝熟悉的气味。匆忙的街头与雨滴的搭配总是毫无违和的一种别样曼妙。"
+	},
+	{
+		"src":"static/img/05/1.jpg",
+		"longitude": 43.000,
+		"latitude": 42.000,
+		"title": "云雾",
+		"equipment":'Nikon D5 + Zeiss 150-600mm F5-6.3 PG OS HSM',
+		"origin": "格鲁吉亚",
+		"author": "George Khelashvili",
+		"description": "黑海与里海之间的格鲁吉亚是一个陌生的国度，卡兹别吉(Kazbegi)是格鲁吉亚和俄罗斯边境交界处的小镇。Kazbegi海拔5033米，是高加索深处的世外桃源。（作者：George Khelashvili2018-04-17）"
+	},
+	{
+		"src":"static/img/06/1.jpg",
+		"longitude": 6.000,
+		"latitude": 45.000,
+		"title": "阿尔卑斯山河谷",
+		"equipment":'Canon 5D4 + Zeiss Otus 1.4/85mm ZE',
+		"origin": "意大利",
+		"author": "Guerel Sahin",
+		"description": "这组令人惊叹的意大利自然风光是由摄影师Guerel Sahin所拍摄，Guerel Sahin的摄影技术是完全通过自学的手段完成，他现在居住在德国的凯泽斯劳滕，并且他将更多的经历是旅游和摄影方面。（作者：Guerel Sahin2018-07-13）"
+	},
+	{
+		"src":"static/img/07/1.jpg",
+		"longitude": 140.000,
+		"latitude": -30.000,
+		"title": "海浪",
+		"equipment":'Nikon D5300 + SIGMA Art 24-70/2.8 DG OS HSM',
+		"origin": "澳大利亚新南威尔士州",
+		"author": "Warren Keelan",
+		"description": "澳大利亚摄影师  沃伦·基兰（Warren Keelan）从旁边捕获了汹涌的浪潮或者涨潮的海水里。在澳大利亚新南威尔士州南海岸的潜水衣中，Warren Keelan穿着潜水衣，拍摄了正在发生的波涛汹涌波涛。（作者：Warren Keelan2017-10-16）"
+	},
+	{
+		"src":"static/img/08/1.jpg",
+		"longitude": 17.000,
+		"latitude": -22.000,
+		"title": "荒原",
+		"equipment":'FUJI X-T20 + XC15-45',
+		"origin": "纳米比亚",
+		"author": "Antti Viitala",
+		"description": "纳米比亚共和国（英语：The Republic of Namibia）位于非洲西南部，北靠赞比亚，东连博茨瓦纳，南接南非。海拔高度为1000-2000米，干旱少雨，属亚热带、半沙漠性气候。（作者：Antti Viitala2017-02-24）"
+	},
+	{
+		"src":"static/img/09/1.jpg",
+		"longitude": 173.000,
+		"latitude": -40.000,
+		"title": "科罗曼德尔半岛",
+		"equipment":'OLYMPUS E-M10 MarkIII + 40-150mm R',
+		"origin": "新西兰",
+		"author": "Craig Lugsden",
+		"description": "科罗曼德，又译科罗曼德尔，科罗曼德半岛位于奥克兰地区的东面，西临豪拉基湾（Hauraki Gulf）)它的田园风光、悠闲自在、朴素自然的科罗曼德尔是新西兰最受欢迎的度假胜地之一。布满岩石的火山丘陵被原始雨林所覆盖。400多公里壮观的海岸线能让你找到梦境般的海滩。（作者：Craig Lugsden2018-10-10）"
+	},
+	{
+		"src":"static/img/10/1.jpg",
+		"longitude": 120.000,
+		"latitude": 30.000,
+		"title": "西湖",
+		"equipment":'SONY ILEC-7K + FE 28-70mm F3.5-5.6',
+		"origin": "中国杭州",
+		"author": "董帅",
+		"description": "从1月3日开始，杭州降下2013年的第一场大雪，连续下了两天两夜，到今日上午刚刚减弱，据说杭州城区积雪最大深度有12厘米。大雪虽然对出行造成了很多不便，却让西湖呈现出了最美的一面。“淡抹浓妆总相宜”，雪后大概就是西湖最最淡雅的状态，跟随我的镜头，一起欣赏银装素裹的美丽“西湖雪”吧。    （图文：锈剑）（作者：董帅2013-01-07）"
+	},
+	{
+		"src":"static/img/11/1.jpg",
+		"longitude": 116.000,
+		"latitude": 40.000,
+		"title": "故宫",
+		"equipment":'Leica S + Leica 35mm F2.5',
+		"origin": "中国北京",
+		"author": "窦瑞冬",
+		"description": "12月25日圣诞节之际，香港兴华拓展有限公司组织数家北京影像媒体在故宫进行了一次徕卡全系列产品的体验试拍活动。参加试拍的机型有M、T、X、D等多个系列的新品。我选择的是全场价格最高的产品——徕卡M搭配徕卡SUMMICRON-M 35mm f/2镜头。相信熟悉徕卡的朋友对这套装备应该不会陌生，2400万像素全画幅CMOS与经典旁轴设计，也是首款支持实时取景与1080p试拍录制的M机型。这枚35mm F2镜头想必也无需过多介绍，在我国摄影人中被赠予“七妹”的昵称，来源于七枚玉的俗称。此次拍摄天气很好，我撷取了9幅光影小品来描绘故宫，拍摄使用DNG格式，后期经LP简单处理。（作者：窦瑞冬2014-12-26）"
+	},
+	{
+		"src":"static/img/12/1.jpg",
+		"longitude": 0.100,
+		"latitude": 51.000,
+		"title": "街景",
+		"equipment":'Pentax 645Z + HDDFA 15-30mm F2.8',
+		"origin": "英国伦敦",
+		"author": "Terry Gibbins",
+		"description": "Terry Gibbins是一位伦敦出租车司机，但在过去的25年中，这位出租司机却并不安于本职工作，而是喜欢在平常开车之余拿起放在车中的相机记录下一路上不同的风景和伦敦市民的生活点滴。当然，Terry Gibbins也并非完全的摄影菜鸟，他曾在大学学习摄影类学科，后来又受到以的士为主题的英国电影《The Knowledge》的影响而成为出租车司机，开始了自己结合兴趣与工作的生活。（作者：Terry Gibbins2018-07-20）"
+	},
+	{
+		"src":"static/img/13/1.jpg",
+		"longitude": 51.000,
+		"latitude": 35.000,
+		"title": "公路之行",
+		"equipment":'Nikon Z7 + 24-70mm',
+		"origin": "伊朗德黑兰",
+		"author": "Paulina Wierzgacz",
+		"description": "在2周的时间里，摄影师和她的朋友围绕伊朗进行了一次长达3000km的公路之旅，从德黑兰到伊斯法罕，再到设拉子、阿巴斯港，以及两个海岛——格什姆岛和霍尔木兹，以及沿途的许多城市，再返回德黑兰。一路同行的只有3个女生，但在路上他们遇到了许多有趣的人们，并且在路途中加入了他们的旅行。最初他们对伊朗一无所知，但经过这次旅行后，才发现沿途的风景非常壮阔，尤其是那些罕为人知的风景。（作者：Paulina Wierzgacz2018-02-11）"
+	},
+	{
+		"src":"static/img/14/1.jpg",
+		"longitude": 80.000,
+		"latitude": 20.000,
+		"title": "平民",
+		"equipment":'Leica S + Summicron-M 50mm/f2.0',
+		"origin": "印度",
+		"author": "Maria Sahai",
+		"description": "这是一次偶然的旅行，摄影师Maria和她的丈夫在周末的时间买了一张廉价的机票，没有任何计划，没有预定，没有搜索和当地有关的任何资讯。她们来到斋普尔后，包了一辆出租车，让司机带领他们走入最质朴、最日常的街头，体验当地居民最平常的一日生活，并且拍下了这些照片。（作者：Maria Sahai2018-03-29）"
+	},
+	{
+		"src":"static/img/15/1.jpg",
+		"longitude": 105.000,
+		"latitude": 15.000,
+		"title": "小镇",
+		"equipment":'Canon 77D + SIGMA 17-50mm f2.8 EX DC OS HSM',
+		"origin": "越南",
+		"author": "Orsolya Karancz",
+		"description": "在摄影师Orsolya心目中，越南是他心仪神往的圣地。越南也经历了许多发展上的转变。这一次，他来到越南北部的山区中，记录下了这些令人惊艳的自然和人文风景。（作者：Orsolya Karancz2018-05-25）"
+	},
+	{
+		"src":"static/img/16/1.jpg",
+		"longitude": 104.000,
+		"latitude": 1.000,
+		"title": "花园城市",
+		"equipment":'SONY ILCE-7RM2 + SEL24-240',
+		"origin": "新加坡",
+		"author": "张磊",
+		"description": "新加坡是一个城市国家，原意为狮城。自1965年独立后，新加坡在40余年内迅速转变成为亚洲四小龙之一。新加坡是个多元种族的移民国家，也是全球最国际化的国家之一。新加坡法律对环境保护的规定非常之严格，也使得新加坡成为世界上最干净的城市之一。让我们一起跟随摄影师的镜头，一起去欣赏一下这个秀丽的城市。 （作者：张磊2013-10-31）"
+	},
+	{
+		"src":"static/img/17/1.jpg",
+		"longitude": 37.000,
+		"latitude": 55.000,
+		"title": "大雪纷飞",
+		"equipment":'Pentax K11 + MARK2 + HD DA 55-300mm PLM WR',
+		"origin": "俄罗斯莫斯科",
+		"author": "Кристина Макеева",
+		"description": "最近几天，俄罗斯大雪纷飞，莫斯科街头银装素裹。当地传统的建筑风格在大雪的笼罩下犹如童话一般迷人绚丽。摄影师带着相机不畏严寒走上街头，借助夜晚的灯光和闪光灯的辅助，拍摄了一些列吸引眼球的视觉大片。雪永远是梦境和魔幻的最佳拍档。（作者：Кристина Макеева2018-02-09）"
+	},
+	{
+		"src":"static/img/18/1.jpg",
+		"longitude": -140.000,
+		"latitude": 35.000,
+		"title": "春樱",
+		"equipment":'OLYMPUS EPL8 + 14-42mm',
+		"origin": "日本",
+		"author": "董帅",
+		"description": "3月份已经到来，这是个春回大地、繁花即将盛开的月份，日本的樱花在这个季节将会陆续开放。据日本气象协会发表的2013樱花花期预测，2月回暖，在樱花开放较早的九州至东海地区，预计会有不少地方提前迎来花期。九州、四国等地的樱花预计自3月中旬起绽放。九州至东海地区花期预计与往年持平或提前，关东至北海道地区则与往年持平。应该不至于出现像去年赏花佳期大幅延后的现象。    下面是编辑精心挑选的日本樱花最美的摄影图片，让我们先来一睹为快。（作者：董帅2013-03-02）"
+	},
+	{
+		"src":"static/img/19/1.jpg",
+		"longitude": -100.000,
+		"latitude": -80.000,
+		"title": "冰川",
+		"equipment":'Nikon D7500 + Zeiss Milvus 100mm f/2.0 ZF.2',
+		"origin": "南极",
+		"author": "Henri Cartier-Bresson",
+		"description": "南极洲充满了神秘但并非不可征服，越来越多的科考队和考察团来到南极，随行的摄影师们拍下了南极洲的种种奇观。热水抛洒到零下三十度的空气中，结成水汽和冰晶；海面上出现冰山的海市蜃楼；傍晚时分梦幻的五彩祥云；极光和企鹅在这里都是寻常的景观；巨大的鲸鱼化石让人瞠目结舌；这就是遥远的南极。（作者：大西洋月刊2015-12-02）"
+	},
+	{
+		"src":"static/img/20/1.jpg",
+		"longitude": -79.000,
+		"latitude": 43.000,
+		"title": "夜景",
+		"equipment":'Canon EOS M50 15-25',
+		"origin": "加拿大多伦多",
+		"author": "Bora Green",
+		"description": "来自多伦多的摄影师Bora擅长拍摄都市风景，尤其是夜景的把控更是非凡。镜头下，城市霓虹灯、广告牌、玻璃的反光变成了夜空的颜料和笔触，加上各种天气的气氛渲染，勾勒出一幅幅迷人的城市印象派作品……（作者：Bora2018-01-26）"
+	}
+];
